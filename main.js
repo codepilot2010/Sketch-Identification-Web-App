c585422420aec3001d7b6f99213cc6fefdb4c29a
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2023-03-31 20:39:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,44 @@ function setup()
     canvas=createCanvas(280,280);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
 }
 
 function clearcanvas()
 {
     background("white");
+}
+
+function preload()
+{
+    classifier=ml5.imageClassifier('DoodleNet');
+}
+
+function draw()
+{
+    strokeWeight(13);
+    stroke(0);
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("label").innerHTML="label : "+results[0].label;
+        document.getElementById("confidence").innerHTML="confidence : "+Math.round(results[0].confidence*100)+"%";
+        utterthis=new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utterthis);
+    }
 }
